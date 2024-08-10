3d800286f64f2615be0425a7f681ebf00fb6fc91
Shoffly <80728185+Shoffly@users.noreply.github.com>
2024-08-10 16:47:33 +0000

    added link to nav

--- components/Nav.js
+++ components/Nav.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import styles from '../styles/nav.module.css';
+import Link from 'next/link';
 
 export default function Nav() {
   return (
     <nav className={styles.nav}>
+      <Link href="/">
       <div className={styles.logo}>
         Nonito<span className={styles.dot}>.</span>
       </div>
+        </Link>
     </nav>
   );
 }
