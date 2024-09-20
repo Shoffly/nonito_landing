2cbbfe36bb3f932cba021f76da46ed2f23bc08a0
shoffly <mohammed09ahmed@gmail.com>
2024-09-20 04:17:04 +0400

    added posthog analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,24 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import posthog from "posthog-js"
+import { PostHogProvider } from 'posthog-js/react'
+
+if (typeof window !== 'undefined') {
+  posthog.init('phc_6d3b8MReLeK1h1ZYYPzatmDF4o9kHXedOwd4sUo9Y90', {
+    api_host: 'https://us.i.posthog.com',
+    person_profiles: 'identified_only',
+    loaded: (posthog) => {
+      if (process.env.NODE_ENV === 'development') posthog.debug()
+    },
+  })
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <PostHogProvider client={posthog}>
+      <Component {...pageProps} />
+    </PostHogProvider>
+  )
 }
 
 export default MyApp
