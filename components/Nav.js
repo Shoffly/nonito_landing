bf6ed443d9351d4903c34d4d64083636e412a5f1
shoffly <mohammed09ahmed@gmail.com>
2024-09-21 00:49:38 +0400

    hamburger menu

--- components/Nav.js
+++ components/Nav.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from '../styles/nav.module.css';
 import Link from 'next/link';
 
 export default function Nav() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <nav className={styles.nav}>
       <Link href="/">
@@ -10,7 +16,12 @@ export default function Nav() {
           Nonito<span className={styles.dot}>.</span>
         </div>
       </Link>
-      <div className={styles.navLinks}>
+      <div className={styles.hamburger} onClick={toggleMenu}>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+      <div className={`${styles.navLinks} ${isMenuOpen ? styles.open : ''}`}>
         <Link href="/pricing">
           <div className={styles.navButton}>Pricing</div>
         </Link>
