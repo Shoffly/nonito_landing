e60a9609d93dfcc3810eb1360bc69ea4ad58bb6b
shoffly <mohammed09ahmed@gmail.com>
2024-12-21 15:19:06 +0400

    twitter ignored for now

--- components/SEO.js
+++ components/SEO.js
@@ -33,7 +33,7 @@ export default function SEO({
 
       {/* Twitter */}
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:site" content={twitterHandle} />
+     
    
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
