071b4cb62bc14f90c4c5eb00c2ce3814b308abe7
Shoffly <80728185+Shoffly@users.noreply.github.com>
2025-03-12 08:58:17 +0200

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import Nav from '../components/Nav';
 import Footer from '../components/footer';
-import styles from '../styles/About.module.css';
+
 
 export default function About() {
     return (
